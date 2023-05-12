176934f7436efec3ae15148ccf16c8020fde3dca
wahtej <125274697+wahtej@users.noreply.github.com>
2023-05-12 16:06:11 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,18 @@
+variable "vpc_id" {
+    type = string
+}
+
+variable "ami_id" {
+    type = string
+}
+
+variable "instance_type" {
+    type = string
+}
+
+variable "subnet_id" {
+    type = string
+}
+variable "instance_name" {
+    type = string
+}
